820bf203ebb7f4e53b1bc104af9b9a8e8a2dbc54
Christian Gould <cagould@smu.edu>
2023-05-06 12:14:38 -0500

    updated requirements and added mongodb

--- backend/backend.py
+++ backend/backend.py
@@ -1,34 +1,23 @@
-from flask import Flask, request
-from flask_restful import Resource, Api, reqparse, request
-from flask_cors import CORS
-from flask_sqlalchemy import SQLAlchemy
+import pymongo
+from flask import Flask
 
 app = Flask(__name__)
-CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
 
-# Connected to sqlite database for now, will be changed later
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['SECRET_KEY'] = 'secret'
-api = Api(app)
-db = SQLAlchemy(app)
+# Connect to MongoDB
+username = '<your-db-username>'
+password = '<your-db-password>'
+host = '<your-db-host>'
+dbname = '<your-db-name>'
 
-class Test(Resource):
-   def get(self):
-    return {
-        "statusCode": 200,
-        "body": {"data": "Hello there, welcome to the test"},
-        "headers": {
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Credentials": True,
-          "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
-          "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token"
-        },
-      } 
+client = pymongo.MongoClient(f'mongodb+srv://{username}:{password}@{host}/{dbname}?retryWrites=true&w=majority')
+db = client[dbname]
 
-api.add_resource(Test, '/test')
+# Sample endpoint to retrieve all users
+@app.route('/users')
+def get_users():
+    users = db.users.find()
+    return {'users': users}
 
 if __name__ == '__main__':
-    db.create_all()
     app.run(host="0.0.0.0", port=8080)  # run our Flask app
+
